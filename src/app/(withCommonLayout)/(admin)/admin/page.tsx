c91d25ac4ef38521ccb9ff8b58289cc1c0fe9575
Sha Fin <99030043+shshafin@users.noreply.github.com>
2025-08-29 21:07:39 +0600

    force dynamic

--- src/app/(withCommonLayout)/(admin)/admin/page.tsx
+++ src/app/(withCommonLayout)/(admin)/admin/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Suspense } from "react";
 import GlassCardsCollection from "./_components/GlassCardsCollection";
 
--- src/app/(withCommonLayout)/(user)/profile/page.tsx
+++ src/app/(withCommonLayout)/(user)/profile/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Suspense } from "react";
 
 import LatestSystemActivitiesSkeleton from "../../(admin)/admin/_components/LatestSystemActivitiesSkeleton";
